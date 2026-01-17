bc9e92acc808952bed4ac4c8f6e5b2a094662bb1
01takhshish <takhshish.531@gmail.com>
2026-01-17 14:49:39 +0530

    cors

--- config.js
+++ config.js
@@ -11,12 +11,6 @@ const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   
   corsOrigins: process.env.CORS_ORIGINS 
-    ? process.env.CORS_ORIGINS.split(',').map(origin => origin.trim())
-    : [
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:5173',
-      ],
 };
 
 export default config;
